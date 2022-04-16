244530c677dc0ab9c5c6c303f40762aed8db462e
shakilahmed09 <shakilahmed.pure@gmail.com>
2022-04-17 02:11:30 +0600

    navbar responsive complete

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css'
 import logo from '../../Images/logo.png'
 import CustomLink from '../CustomLink/CustomLink';
 import { Link, useNavigate } from 'react-router-dom';
+import { MenuIcon, XIcon } from '@heroicons/react/solid'
 
 
 const Header = () => {
+    const [open, setOpen] = useState(false)
     const navigate = useNavigate()
 
     const navigateToLoginPage = () => {
@@ -13,10 +15,15 @@ const Header = () => {
     }
     return (
         <div className='header-container'>
-            <div>
-                <Link to="/" className='text-white font-bold text-2xl cursor-pointer website-title'>Center of Strength</Link>
+            <div className='flex items-center justify-between'>
+                <div>
+                    <Link to="/" className='text-white font-bold text-2xl cursor-pointer website-title'>Center of Strength</Link>
+                </div>
+                <div onClick={() => setOpen(!open)} className='menu-icon'>
+                    {open ? <XIcon></XIcon> : <MenuIcon></MenuIcon>}
+                </div>
             </div>
-            <div className='header-menu'>
+            <div className={`header-menu absolute ${open ? "right-10" : "top-[-200px]"}`}>
                 <CustomLink to="/">Home</CustomLink>
                 <CustomLink to="/blogs">Blogs</CustomLink>
                 <CustomLink to="/about">About Me</CustomLink>
