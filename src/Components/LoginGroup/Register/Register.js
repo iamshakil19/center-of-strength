4c68cdb650ecc76543b3dcdb17c70663771f703d
shakilahmed09 <shakilahmed.pure@gmail.com>
2022-04-17 04:59:45 +0600

    register page complete

--- src/Components/LoginGroup/Register/Register.js
+++ src/Components/LoginGroup/Register/Register.js
@@ -1,9 +1,73 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import SocialLogin from '../../SocialLogin/SocialLogin';
+import loginImage from '../../../Images/login-image.jpg'
 import './Register.css'
+
+
 const Register = () => {
+    const handleSubmit = event => {
+        event.preventDefault()
+    }
     return (
         <div>
-            <h2 className='text-white'>This is register page</h2>
+            <h2 className='text-white text-center my-10 text-4xl font-bold font-extralight'>Register</h2>
+            <div className='login-container'>
+                <form onClick={handleSubmit} className='inputs-field'>
+                    <div className='flex justify-center'>
+                        <div className=''>
+                            <p className='text-white mb-2'>Name</p>
+                            <input className='outline-0 bg-transparent text-white input-style ' type="text" name="name" id="name" placeholder='Type your name' required />
+                            <div style={{ height: "1px", width: "280px" }} className="bg-slate-400"></div>
+                        </div>
+                    </div>
+                    
+                    <div className='flex justify-center'>
+                        <div className=''>
+                            <p className='text-white mb-2 mt-5'>Email</p>
+                            <input className='outline-0 bg-transparent text-white input-style ' type="email" name="email" id="email" placeholder='Type your email' required />
+                            <div style={{ height: "1px", width: "280px" }} className="bg-slate-400"></div>
+                        </div>
+                    </div>
+
+                    <div className='flex justify-center'>
+                        <div className=''>
+                            <p className='text-white mb-2 mt-5'>Password</p>
+                            <input className='outline-0 bg-transparent text-white input-style ' type="password" name="password" id="password" placeholder='Type your password' required />
+                            <div style={{ height: "1px", width: "280px" }} className="bg-slate-400"></div>
+                        </div>
+                    </div>
+
+                    <div className='flex justify-center'>
+                        <div className=''>
+                            <p className='text-white mb-2 mt-5'>Confirm Password</p>
+                            <input className='outline-0 bg-transparent text-white input-style ' type="password" name="confirm-password" id="confirm-password" placeholder='Type your confirm password' required />
+                            <div style={{ height: "1px", width: "280px" }} className="bg-slate-400"></div>
+                        </div>
+                    </div>
+
+                    <div className='flex justify-center mt-4'>
+                        <div>
+                            <p className=''><span className='text-white'>Already have an account ? <Link to="/login" ><span className='text-blue-500'>Login</span></Link></span></p>
+                        </div>
+                    </div>
+
+                    <div className='flex justify-center mt-3'>
+                        <div>
+                            <p className=''><Link to="/" ><span className='text-blue-500'>Forgot Password ?</span></Link></p>
+                        </div>
+                    </div>
+
+                    <div className='flex justify-center my-7'>
+                        <input className='text-white text-lg font-semibold border py-1 px-4 rounded hover:bg-stone-800 duration-700' type="submit" value="Login" />
+                    </div>
+                    <p className='text-white text-center'>Or login with</p>
+                    <SocialLogin></SocialLogin>
+                </form>
+                <div>
+                    <img className='login-image rounded-3xl' src={loginImage} alt="" />
+                </div>
+            </div>
         </div>
     );
 };
